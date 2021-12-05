b48dedcab6543ff0424644f8dfdd393165228079
Mateo Piccinini <mateopiccinini2004@gmail.com>
2021-12-05 17:13:51 -0300

    header de index

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public')
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.listen(420, function(){
+    console.log('servidor corriendo');
+});
