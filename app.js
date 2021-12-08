af51b235e2cf77a078d04e9db624922753ad9e52
Zabala Saint Paul <zabalasaintpaul@gmail.com>
2021-12-08 16:27:20 -0300

    Zaba Subiendo Avances del ProductDetail

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.get('/register', function(req,res){
 app.get ('/login', function (req,res){
     res.sendFile (path.join(__dirname, './views/login.html'))
 });
+app.get ('/productDetail', function (req,res){
+    res.sendFile (path.join(__dirname, './views/productDetail.html'))
+});
 
 app.listen(420, function(){
     console.log('servidor corriendo');
