fbe0097553c936b4e94766d534c541a8e7874414
Mateo Piccinini <mateopiccinini2004@gmail.com>
2022-01-20 19:11:38 -0300

    app.js funciona afuera de src

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ const path = require('path');
 
 
 //Importo el de Routes el enrutador del index en este caso mi pantalla principal
-const productsRoute = require('./../src/routes/productsRoute');
-const usersRoute = require('./../src/routes/usersRoute');
+const productsRoute = require('./src/routes/productsRoute');
+const usersRoute = require('./src/routes/usersRoute');
 
 //Defino app para usar las funciones de express
 const app = express();
@@ -59,15 +59,11 @@ app.use('/users', usersRoute)
 
 //Para parasrse en carpeta public
 
-app.use(express.static(path.resolve(__dirname, '..','./public')));
-app.use(express.static(path.resolve(__dirname, '..','./views')));
-
-
-
+app.use(express.static(path.resolve(__dirname, './public')));
 
 //Para que funcione ejs
+app.set('views','./src/views')
 app.set('view engine', 'ejs')
-
 
 //Para levantar servidor con heroku
 app.listen(process.env.PORT || 420, function(){
